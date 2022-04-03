7c284814b8b197903c0136bad6a36a0402073e0d
daniel zamora <daniel.zamora@opendeusto.es>
2022-04-04 00:00:54 +0200

    Remove ignored files

--- 2. Codes/upload_data_and_create_charts.bat
+++
@@ -1,5 +0,0 @@
-call C:\Users\danie\anaconda3\Scripts\activate.bat pysandbox
-python ".\2. Codes\load_idealista_data_to_BBDD.py"
-python ".\2. Codes\create_idealista_renting_chart.py"
-conda deactivate
-cmd \k
